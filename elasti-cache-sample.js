5789c4f12b9435e49a63c0ed91e1507730348f05
KeitaNishimoto <k.mail.09070876640@icloud.com>
2016-12-19 02:09:21 +0900

    modify #17 インターネット接続を試す為の仮実装

--- elasti-cache-sample.js
+++ elasti-cache-sample.js
@@ -1,4 +1,6 @@
 const request = require('request');
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
 const headers = {
   'Content-Type':'application/json'
@@ -10,6 +12,31 @@ const options = {
   headers: headers
 };
 
-request(options, (error, response, body) => {
-  console.log(response);
-});
+exports.handler = (event, context, callback) => {
+
+  /*
+  request(options, (error, response, body) => {
+    console.log(body);
+    callback(
+      null,
+      response
+    );
+  });
+  */
+
+  const params = {
+    TableName: 'access_tokens',
+    FilterExpression : 'access_token = :val',
+    ExpressionAttributeValues : {':val': 'e4459aca-f01e-4a27-add0-05181ddc4122'}
+  };
+
+  dynamo.scan(params, (error, data) => {
+    if (error) {
+      callback(
+        Error('Fail. err:' + error)
+      );
+    } else {
+      callback(null, data);
+    }
+  });
+};
